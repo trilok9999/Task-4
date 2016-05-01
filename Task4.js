98c23cadc9276b5ffdcf2b41302ab5f92dc65eb5
trilok9999 <s521950@mail.nwmissouri.edu>
2016-05-01 17:36:29 -0400

    done

---
+++ Task4.js
@@ -0,0 +1,36 @@
+/**
+ * Created by trilok on 5/1/2016.
+ */
+
+function sort(strng){
+    var map={};
+    var split_arr=strng.split(" "); //spliting the string
+    for(var i=0;i<split_arr.length;i++){
+        var number_arr=split_arr[i].split(""); //spliting the number to calculate weight
+        var sum=0;
+        for(var j=0;j<number_arr.length;j++){
+            sum+=parseInt(number_arr[j]);
+        }
+        // checking whether we have weight already existing
+        if (map.hasOwnProperty(sum)) {
+            map[sum].push(number_arr.join(""))
+        }
+        else
+            map[sum] = [number_arr.join("")];
+    }
+
+    var sorted_map=[];
+    //sorting the equal weighed numbers according to their length in descending order(if weight is equal. we sort according to the length)
+    for (var key in map) {
+        if (map.hasOwnProperty(key)) {
+            sorted_map.push([key,map[key].sort(function(a,b){
+                return -(a.length- b.length);
+            })]);
+        }
+    }
+    return sorted_map.map(function(a){
+        return a[1].join(" ");
+    }).join(" ");
+}
+console.log(sort("350 44 67 90 9 11 11 2 20 2000"));
+console.log(sort("450 1 1 001 2 99 099 0099"));
